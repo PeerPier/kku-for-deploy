036a3d2086a2efeaa17586a36af61e802bff4ccd
PeerPier <pzgp12@gmail.com>
2025-02-24 03:14:39 +0700

    update email send to support enable/disable notification

--- server/mail/noti_sender.js
+++ server/mail/noti_sender.js
@@ -12,47 +12,53 @@ const NotiMailer = async (targetId,actionById,userAction,actionId,reason) => {
 
     const findTargetUser = await User.findById(targetId);
     const findActionUser = actionById=='System'?'System':await User.findById(actionById);
+    console.log("accept email: ",findTargetUser.notification_email_enable);
+
+    if(findTargetUser.notification_email_enable){
+        const to = findTargetUser.email;
+        const toName = findTargetUser.username;
+        const actionByName = actionById=='System'?'':findActionUser.username;
+        let actionTopic = null;
+        let actionDirect = null;    
+        
+        if(actionId && (userAction == 'like' || userAction == 'comment' || userAction == 'reply')){
+            const find = await Blog.findById(actionId);
+            actionTopic = `บล็อก ${find.topic} ของคุณ`;
+            actionDirect = `${process.env.FRONTEND_ENDPOINT}/blog/${find.blog_id}`;
+        }
+        else if(userAction == 'follow'){
+            actionTopic = "โปรไฟล์ของคุณ"
+            actionDirect = `${process.env.FRONTEND_ENDPOINT}/user/${actionById}`
+        }else if(userAction == 'delete'){
+            actionTopic = `ระบบได้ลบบล็อก ${actionId} ของคุณเนื่องจาก ${reason}`
+            actionDirect = `${process.env.FRONTEND_ENDPOINT}/dashboard/notifications`
+        }
     
-    const to = findTargetUser.email;
-    const toName = findTargetUser.username;
-    const actionByName = actionById=='System'?'':findActionUser.username;
-    let actionTopic = null;
-    let actionDirect = null;    
+        const mailOptions = {
+            from: process.env.MAIL_USER,
+            to: to,
+            subject: `[KKU Blogging] แจ้งเตือน ${capUserAction}`,
+            html: Template(toName,capUserAction,actionByName,actionTopic,actionDirect),
+            attachments: [
+                {
+                    filename: 'logo.png',
+                    path: logoPath,
+                    cid: 'logo'
+                }
+            ]
+        };
     
-    if(actionId && (userAction == 'like' || userAction == 'comment' || userAction == 'reply')){
-        const find = await Blog.findById(actionId);
-        actionTopic = `บล็อก ${find.topic} ของคุณ`;
-        actionDirect = `${process.env.FRONTEND_ENDPOINT}/blog/${find.blog_id}`;
-    }
-    else if(userAction == 'follow'){
-        actionTopic = "โปรไฟล์ของคุณ"
-        actionDirect = `${process.env.FRONTEND_ENDPOINT}/user/${actionById}`
-    }else if(userAction == 'delete'){
-        actionTopic = `ระบบได้ลบบล็อก ${actionId} ของคุณเนื่องจาก ${reason}`
-        actionDirect = `${process.env.FRONTEND_ENDPOINT}/dashboard/notifications`
+        try {
+            const info = await transporter.sendMail(mailOptions);
+            console.log('Email sent: ' + info.response,"SUCCESS");
+            console.log("TO: ",to)
+        } catch (error) {
+            console.error('Error sending email:', error);
+        }
+    }else{
+        console.log("Not accept email, Cancel send email");
     }
 
-    const mailOptions = {
-        from: process.env.MAIL_USER,
-        to: to,
-        subject: `[KKU Blogging] แจ้งเตือน ${capUserAction}`,
-        html: Template(toName,capUserAction,actionByName,actionTopic,actionDirect),
-        attachments: [
-            {
-                filename: 'logo.png',
-                path: logoPath,
-                cid: 'logo'
-            }
-        ]
-    };
-
-    try {
-        const info = await transporter.sendMail(mailOptions);
-        console.log('Email sent: ' + info.response,"SUCCESS");
-        console.log("TO: ",to)
-    } catch (error) {
-        console.error('Error sending email:', error);
-    }
 };
 
 module.exports = { NotiMailer };
